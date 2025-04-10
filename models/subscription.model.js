5531ab8228f1543ecb7f7c5a3ad22f5cbdcd6240
Trịnh Quang Minh <tqminhhcm@gmail.com>
2025-04-10 14:24:21 +0700

    Half way trying to connect to the Database.

---
+++ models/subscription.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const subcriptionSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Subscription name is required'],
+        trim: true,
+        minLength: 2,
+        maxLength: 100,
+    },
+    price: {
+        type: Number,
+        required: [true, 'Subscription price is required'],
+        min: [0, 'Price must be greater than 0'],
+    },
+    currency: {
+        type: Number,
+        enum: ['EUR', 'USD', 'VND', 'GBP'],
+        default: 'USD',
+    },
+    frequency: {
+        type: String,
+        enum: ['daily', 'weekly', 'monthly', 'yearly'],
+    },
+    category: {
+        type: String,
+        enum: ['sport', 'news', 'entertainment', 'technology', 'finance', 'politics', 'other'],
+        required: true,
+    }
+}, {timestamps: true});
---
+++ models/user.model.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'User name is required'],
+        trim: true,
+        minLength: 2,
+        maxLength: 50,
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        trim: true,
+        unique: true,
+        lowercase: true,
+        match: [/\S+@\S+\.\S+/, "Please enter valid email address"],
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+        minLength: 6,
+    }
+}, {timestamp:true});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
